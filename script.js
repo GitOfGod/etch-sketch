5d31754825dc7e7db8cc5c3b4f90a9908e6d2186
Favour <favouramike@gmail.com>
2024-05-21 14:43:04 +0100

    feat: Create function for creating divs
    - Function takes class name as parameter to allow divs from different
    classes to be created

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector(".container");
+
+function createDiv(className) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add(className);
+    return newDiv;
+}
